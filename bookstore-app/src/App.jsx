90ef2bb36de94f2c57cc9bfa2fc2bc2a4b05766e
Jennifer Zhuang <118019266+jzhuang03@users.noreply.github.com>
2024-04-13 15:19:57 -0400

    working code

---
+++ bookstore-app/src/App.jsx
@@ -0,0 +1,32 @@
+import "./App.css";
+import { GenreSelector } from "./components/GenreSelector";
+
+
+/* Provided code from document */
+function App() {
+  const bookData = {
+    fiction: [
+      { title: "The Great Gatsby", author: "F. Scott Fitzgerald", price: 10.0 },
+      { title: "1984", author: "George Orwell", price: 8.5 },
+      { title: "The Catcher in the Rye", author: "J.D. Salinger", price: 9.8 },
+    ],
+    "non-fiction": [
+      { title: "Sapiens: A Brief History of Humankind", author: "Yuval Noah Harari", price: 15.0 },
+      { title: "In Cold Blood", author: "Truman Capote", price: 12.0 },
+      { title: "The Diary of a Young Girl", author: "Anne Frank", price: 7.0 },
+    ],
+    children: [
+      { title: "Charlotte's Web", author: "E.B. White", price: 5.0 },
+      { title: "The Gruffalo", author: "Julia Donaldson", price: 6.0 },
+      { title: "Where the Wild Things Are", author: "Maurice Sendak", price: 8.0 },
+    ],
+  };
+
+  return (
+    <>
+      <GenreSelector data={bookData} />
+    </>
+  );
+}
+
+export default App;
